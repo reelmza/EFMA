c5bd6b05a6beac15298613eb7b5c0efc7b71698f
reelmza <jessemoses71@gmail.com>
2010-01-01 22:11:08 +0100

    Added some pages/tweaks

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+
+const sidebarBtn = document.getElementsByClassName('bar-icon')[0]
+const sidebar_dropdown = document.getElementsByClassName('mobile-nav-dropdown')[0]
+
+const searchBtn = document.getElementsByClassName('search-icon')[0]
+const searchInput = document.getElementById('site-search')
+
+let sidebar_state = 0
+let search_state = 0
+
+sidebarBtn.addEventListener('click', function () {
+    console.log('Sidebar Click')
+    if (sidebar_state == 0) {
+        sidebar_dropdown.style.height = '90vmin'
+        sidebar_dropdown.style.top = "16vh"
+        return sidebar_state = 1
+    }
+    sidebar_dropdown.style.height = '0px'
+    sidebar_dropdown.style.top = "-14vh"
+    return sidebar_state = 0
+})
+
+searchBtn.addEventListener('click', function () {
+    console.log('Search Click')
+    if (search_state == 0) {
+        searchInput.style.right = '0'
+        searchBtn.style.marginRight = '48vmin'
+        return search_state = 1
+    }
+
+    searchInput.style.right = null
+    searchBtn.style.marginRight = null
+    return search_state = 0
+})

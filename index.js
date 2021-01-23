84d69b12ff19db4fd559b1e7f0d9256c781b5e5a
reelmza <jessemoses71@gmail.com>
2021-01-23 14:28:26 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const sidebar = document.getElementsByClassName('bar-icon')[0]
+const sidebar_dropdown = document.getElementsByClassName('mobile-nav-dropdown')[0]
+
+let sidebar_state = 0
+sidebar.addEventListener('click', function () {
+    console.log('click')
+    if (sidebar_state == 0) {
+        sidebar_dropdown.style.height = '40vh'
+        sidebar_dropdown.style.opacity = "1"
+        return sidebar_state = 1
+    }
+    sidebar_dropdown.style.height = '0px'
+    sidebar_dropdown.style.opacity = "0"
+    return sidebar_state = 0
+})
